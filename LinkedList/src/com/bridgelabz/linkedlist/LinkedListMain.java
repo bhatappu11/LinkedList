70572b49bc6ab0d9377f00d806ea0b964088e389
Arpitha Bhat <arpithabht12@gmail.com>
2021-08-31 22:45:48 +0530

    MODIFIED: changed main to use insert

--- LinkedList/src/com/bridgelabz/linkedlist/LinkedListMain.java
+++ LinkedList/src/com/bridgelabz/linkedlist/LinkedListMain.java
@@ -5,8 +5,8 @@ public class LinkedListMain {
 	public static void main(String[] args) {
 		LinkedList<Integer> linkedList = new LinkedList<>();
 		linkedList.append(new Node(56));
-		linkedList.append(new Node(30));
 		linkedList.append(new Node(70));
+		linkedList.insert(new Node(30),56);
 		
 		linkedList.printNodes();
 	}
