38bd57973b4848c445014e2ef05690084ac40e97
Arpitha Bhat <arpithabht12@gmail.com>
2021-08-31 21:40:06 +0530

    ADDED:created linked list with three nodes

---
+++ LinkedList/src/com/bridgelabz/linkedlist/LinkedList.java
@@ -0,0 +1,13 @@
+package com.bridgelabz.linkedlist;
+
+public class LinkedList {
+	Node head;
+	Node tail;
+	
+	public LinkedList() {
+		this.head = null;
+		this.tail = null;
+	}
+	
+	
+}
--- LinkedList/src/com/bridgelabz/linkedlist/LinkedListMain.java
+++ LinkedList/src/com/bridgelabz/linkedlist/LinkedListMain.java
@@ -3,8 +3,13 @@ package com.bridgelabz.linkedlist;
 public class LinkedListMain {
 
 	public static void main(String[] args) {
+		Node<Integer> firstNode = new Node<>(56); 
+		Node<Integer> secondNode = new Node<>(30); 
+		Node<Integer> thirdNode = new Node<>(70); 
 		
-
+		firstNode.setNext(secondNode);
+		secondNode.setNext(thirdNode);
+		System.out.println(firstNode.getKey()+"->"+secondNode.getKey()+"->"+thirdNode.getKey());
 	}
 
 }
